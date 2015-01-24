1a27bffd882fc33ee514c4e44ba364b0e7610711
philipjfleger <philipjfleger@gmail.com>
2015-01-24 17:31:27 -0500

    fill in cacheSolve function

--- cachematrix.R
+++ cachematrix.R
@@ -21,6 +21,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) 
+{
+  m <- x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
